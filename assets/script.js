8c2035e113c377ccbe9478db698fafb1fb872487
Ninamahdawe <ninamahdawe11@gmail.com>
2023-07-06 13:02:15 -1000

    added code to script.js

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+var question = [
+    {
+        question: "Which of the following is a programming language?",
+        answer: [
+            { text: "HTML", correct: true },
+            { text: "Shark", correct: false },
+            { text: "Google", correct: false },
+            { text: "Adope scan", correct: false },
+        ]
+    },
+    {
+        question: "What does the CSS acronym stand for?",
+        answer: [
+            { text: "Cascading Style Sheets", correct: true },
+            { text: "Float", correct: false },
+            { text: "String", correct: false },
+            { text: ".Txt", correct: false },
+        ]
+    },
+    {
+        question: "Which operator is used for concatenation in most programming languages?",
+        answer: [
+            { text: "/", correct: false },
+            { text: "+", correct: true },
+            { text: "%", correct: false },
+            { text: "boolean", correct: false },
+        ]
+    },
+    {
+        question: "Which keyword is used to define a function in JavaScript? ",
+        answer: [
+            { text: "Def", correct: false },
+            { text: "Python", correct: false },
+            { text: "Return", correct: false },
+            { text: "function", correct: true },
+        ]
+    }
+]
